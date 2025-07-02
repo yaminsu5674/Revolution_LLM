efdd188ffe596e1e314778f313e7909af4f9a597
kim minsu <yaminsu5674@gmail.com>
2025-07-02 21:48:29 +0900

    feat: init GPTManager class

---
+++ Source/Revolution_LLM/Public/GPT/GPTManager.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "GPTManager.generated.h"
+
+/**
+ * 
+ */
+
+// minsu's annotation ----------------------------------------
+// it is manager who acts like world variables
+// it contatins api functions
+// it manages waiting api response
+// it contatins states related with all things of GPT api
+
+UCLASS()
+class REVOLUTION_LLM_API UGPTManager : public UObject
+{
+	GENERATED_BODY()
+	
+};
