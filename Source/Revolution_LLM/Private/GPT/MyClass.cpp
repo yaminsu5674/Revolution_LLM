3c36587d6c2735241e43623385c9011aefe7a657
MegasKomnenos <35184490+MegasKomnenos@users.noreply.github.com>
2025-07-03 15:37:06 +0900

    foo

---
+++ Source/Revolution_LLM/Private/GPT/MyClass.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GPT/MyClass.h"
+
+MyClass::MyClass()
+{
+	// This is my class
+	// It is mine
+}
+
+MyClass::~MyClass()
+{
+}
