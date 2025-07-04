1251ac40a9011725f35963234cb7b08c66bf38d3
MegasKomnenos <35184490+MegasKomnenos@users.noreply.github.com>
2025-07-04 19:40:33 +0900

    Reapply "Merge branch 'main' of https://github.com/yaminsu5674/Revolution_LLM"
    This reverts commit a87e85266f155bfc8b4ad034e3bce0a8ade000ee.

--- Source/Revolution_LLM/Private/CoreSystems/RevolutionGameInstance.cpp
+++ Source/Revolution_LLM/Private/CoreSystems/RevolutionGameInstance.cpp
@@ -2,4 +2,57 @@
 
 
 #include "CoreSystems/RevolutionGameInstance.h"
+#include "InteractionActors/Suspect.h"
 
+
+void URevolutionGameInstance::SpawnSuspects()
+{
+	UWorld* World = GetWorld();
+	if (!World) return;
+
+	const FString Suspect1Path = TEXT("Blueprint'/Game/Dynamic/Blueprints/Characters/BP_Suspect1.BP_Suspect1_C'");
+	const FString Suspect2Path = TEXT("Blueprint'/Game/Dynamic/Blueprints/Characters/BP_Suspect2.BP_Suspect2_C'");
+	const FString Suspect3Path = TEXT("Blueprint'/Game/Dynamic/Blueprints/Characters/BP_Suspect3.BP_Suspect3_C'");
+
+	UClass* SuspectClass1 = Cast<UClass>(StaticLoadObject(UClass::StaticClass(), nullptr, *Suspect1Path));
+	UClass* SuspectClass2 = Cast<UClass>(StaticLoadObject(UClass::StaticClass(), nullptr, *Suspect2Path));
+	UClass* SuspectClass3 = Cast<UClass>(StaticLoadObject(UClass::StaticClass(), nullptr, *Suspect3Path));
+
+	if (SuspectClass1)
+	{
+		Suspect1 = World->SpawnActor<ASuspect>(SuspectClass1, Suspect1Location, SuspectRotation);
+	}
+	if (SuspectClass2)
+	{
+		Suspect2 = World->SpawnActor<ASuspect>(SuspectClass2, Suspect2Location, SuspectRotation);
+	}
+	if (SuspectClass3)
+	{
+		Suspect3 = World->SpawnActor<ASuspect>(SuspectClass3, Suspect3Location, SuspectRotation);
+	}
+
+	if (Suspect1)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(
+				-1,
+				5.f,
+				FColor::Red,
+				TEXT("Suspect1 init")
+			);
+		}
+	}
+	else
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(
+				-1,
+				5.f,
+				FColor::Red,
+				TEXT("Not yet")
+			);
+		}
+	}
+}
